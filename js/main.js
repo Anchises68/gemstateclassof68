404a1972eb17872006d0b5eb6280439f2fd8cd35
Alex Mireles <anchises1968@gmail.com>
2018-04-03 12:01:13 -0700

    created webpack

--- js/main.js
+++ js/main.js
@@ -1,26 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Bios from './bio.js';
 
-
-const alumni = [
-  {
-    'name': "Noreen",
-    'image': "./images/sample1.jpg",
-    'bio': "Bio info goes here"
-  },
-  {
-    'name': "Noreen",
-    'image': "images/sample1.jpg",
-    'bio': "Bio info goes here"
-  },
-  {
-    'name': "Noreen",
-    'image': "images/sample1.jpg",
-    'bio': "Bio info goes here"
-  },
-]
-
-ReactDOM.render(
-  <Bios bio={alumni}/>,
-  document.getElementById('bio');
+console.log("loaded react-dom");
+console.log("webpack works");
---
+++ mainsaveforlater.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Bios from './bio.js';
+console.log("loaded react-dom");
+
+
+const alumni = [
+  {
+    'name': "Noreen",
+    'image': "./images/sample1.jpg",
+    'bio': "Bio info goes here"
+  },
+  {
+    'name': "Noreen",
+    'image': "images/sample1.jpg",
+    'bio': "Bio info goes here"
+  },
+  {
+    'name': "Noreen",
+    'image': "images/sample1.jpg",
+    'bio': "Bio info goes here"
+  },
+]
+
+ReactDOM.render(
+  <Bios bio={alumni}/>,
+  document.getElementById('bio')
