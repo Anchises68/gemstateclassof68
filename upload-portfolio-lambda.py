0136a19d7c0ec17ff2947e79826c1b3803676575
Alex Mireles <anchises1968@gmail.com>
2018-04-18 12:19:53 -0700

    updated lambda with sns

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -5,19 +5,29 @@ import zipfile
 import mimetypes
 
 def lambda_handler(event, context):
-    s3 = boto3.resource('s3')
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:822374759106:GSAClassof68')
 
-    portfolio_bucket = s3.Bucket('gemstateclassof68.com')
-    build_bucket = s3.Bucket('portfoliobuild.gemstateclassof68.com')
+    try:
+        s3 = boto3.resource('s3')
 
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        portfolio_bucket = s3.Bucket('gemstateclassof68.com')
+        build_bucket = s3.Bucket('portfoliobuild.gemstateclassof68.com')
 
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+        print "Job done"
+        topic.publish(Subject="GSAClassof68", Message="Great Success")
+    except:
+        topic.publish(Subject="GSAClassof68 ERROR!", Message="Upload Error")
+        raise
 
     return 'Hello from Lambda'
